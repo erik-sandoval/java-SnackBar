3b010666e6b06fcbf7df05a860ec3025750aa350
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-09 14:09:53 -0800

    Created customer Class

---
+++ src/javaSnackBar/Customer.java
@@ -0,0 +1,34 @@
+package javaSnackBar;
+
+public class Customer {
+
+    private static int currentId = 0;
+
+    private int id;
+    private String name;
+    private double cash;
+
+    public Customer(String name, double cash){
+
+        currentId++;
+        id = currentId;
+        this.name = name;
+        this.cash = cash;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addCash(double amount){
+        cash += amount;
+    }
+
+    public double getCash() {
+        return cash;
+    }
+}
