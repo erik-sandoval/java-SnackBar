2c848713063adc5588bd7f1900c8405e213d7ba1
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-09 14:10:22 -0800

    Created Vending Machine class

---
+++ src/javaSnackBar/VendingMachine.java
@@ -0,0 +1,32 @@
+package javaSnackBar;
+
+public class VendingMachine {
+
+    private static int currentId = 0;
+
+    private int id;
+    private String name;
+
+    public VendingMachine(String name){
+        currentId += 1;
+
+        id = currentId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
